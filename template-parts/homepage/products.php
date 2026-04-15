4d93e068d98bff383d29c43be52feb8101c4ad8a
paul-bidpixel <paul@bidpixel.com>
2026-04-15 17:13:27 +1000

    design improvement

--- template-parts/homepage/products.php
+++ template-parts/homepage/products.php
@@ -14,53 +14,43 @@ function coffs_product_img( $file ) {
 
 		<div class="products__grid">
 
-			<a class="product-card" href="<?php echo esc_url( home_url( '/chainsaws' ) ); ?>">
-				<img class="product-card__img" src="<?php echo esc_url( coffs_product_img( 'chainsaws-coffs-coast.jpg' ) ); ?>" alt="" width="600" height="400" loading="lazy" decoding="async">
+			<a class="product-card" href="<?php echo esc_url( home_url( '/chainsaws' ) ); ?>" style="background-image: url('<?php echo esc_url( coffs_product_img( 'chainsaws-coffs-coast.jpg' ) ); ?>')">
 				<span class="product-card__label">Chainsaws</span>
 			</a>
 
-			<a class="product-card" href="<?php echo esc_url( home_url( '/lawn-mowers' ) ); ?>">
-				<img class="product-card__img" src="<?php echo esc_url( coffs_product_img( 'lawn-mowers-coffs-coast.jpg' ) ); ?>" alt="" width="600" height="400" loading="lazy" decoding="async">
+			<a class="product-card" href="<?php echo esc_url( home_url( '/lawn-mowers' ) ); ?>" style="background-image: url('<?php echo esc_url( coffs_product_img( 'lawn-mowers-coffs-coast.jpg' ) ); ?>')">
 				<span class="product-card__label">Lawn mowers</span>
 			</a>
 
-			<a class="product-card" href="<?php echo esc_url( home_url( '/ride-on-mowers' ) ); ?>">
-				<img class="product-card__img" src="<?php echo esc_url( coffs_product_img( 'ride-on-mowers-coffs-coast.webp' ) ); ?>" alt="" width="600" height="400" loading="lazy" decoding="async">
+			<a class="product-card" href="<?php echo esc_url( home_url( '/ride-on-mowers' ) ); ?>" style="background-image: url('<?php echo esc_url( coffs_product_img( 'ride-on-mowers-coffs-coast.webp' ) ); ?>')">
 				<span class="product-card__label">Ride-on mowers</span>
 			</a>
 
-			<a class="product-card" href="<?php echo esc_url( home_url( '/zero-turn-mowers' ) ); ?>">
-				<img class="product-card__img" src="<?php echo esc_url( coffs_product_img( 'zero-turn-mowers-gravely-coffs-coast.jpg' ) ); ?>" alt="" width="600" height="400" loading="lazy" decoding="async">
+			<a class="product-card" href="<?php echo esc_url( home_url( '/zero-turn-mowers' ) ); ?>" style="background-image: url('<?php echo esc_url( coffs_product_img( 'zero-turn-mowers-gravely-coffs-coast.jpg' ) ); ?>')">
 				<span class="product-card__label">Zero turn mowers</span>
 			</a>
 
-			<a class="product-card" href="<?php echo esc_url( home_url( '/robot-mowers' ) ); ?>">
-				<img class="product-card__img" src="<?php echo esc_url( coffs_product_img( 'robot-mowers-kress-coffs-coast.jpg' ) ); ?>" alt="" width="600" height="400" loading="lazy" decoding="async">
+			<a class="product-card" href="<?php echo esc_url( home_url( '/robot-mowers' ) ); ?>" style="background-image: url('<?php echo esc_url( coffs_product_img( 'robot-mowers-kress-coffs-coast.jpg' ) ); ?>')">
 				<span class="product-card__label">Robot mowers</span>
 			</a>
 
-			<a class="product-card" href="<?php echo esc_url( home_url( '/brushcutters' ) ); ?>">
-				<img class="product-card__img" src="<?php echo esc_url( coffs_product_img( 'brushcutters-stihl-coffs-coast.webp' ) ); ?>" alt="" width="600" height="400" loading="lazy" decoding="async">
+			<a class="product-card" href="<?php echo esc_url( home_url( '/brushcutters' ) ); ?>" style="background-image: url('<?php echo esc_url( coffs_product_img( 'brushcutters-stihl-coffs-coast.webp' ) ); ?>')">
 				<span class="product-card__label">Brushcutters</span>
 			</a>
 
-			<a class="product-card" href="<?php echo esc_url( home_url( '/generators' ) ); ?>">
-				<img class="product-card__img" src="<?php echo esc_url( coffs_product_img( 'generators-coffs-coast.jpg' ) ); ?>" alt="" width="600" height="400" loading="lazy" decoding="async">
+			<a class="product-card" href="<?php echo esc_url( home_url( '/generators' ) ); ?>" style="background-image: url('<?php echo esc_url( coffs_product_img( 'generators-coffs-coast.jpg' ) ); ?>')">
 				<span class="product-card__label">Generators</span>
 			</a>
 
-			<a class="product-card" href="<?php echo esc_url( home_url( '/pressure-cleaners' ) ); ?>">
-				<img class="product-card__img" src="<?php echo esc_url( coffs_product_img( 'pressure-cleaners-coffs-coast.jpg' ) ); ?>" alt="" width="600" height="400" loading="lazy" decoding="async">
+			<a class="product-card" href="<?php echo esc_url( home_url( '/pressure-cleaners' ) ); ?>" style="background-image: url('<?php echo esc_url( coffs_product_img( 'pressure-cleaners-coffs-coast.jpg' ) ); ?>')">
 				<span class="product-card__label">Pressure cleaners</span>
 			</a>
 
-			<a class="product-card" href="<?php echo esc_url( home_url( '/garden-power-tools' ) ); ?>">
-				<img class="product-card__img" src="<?php echo esc_url( coffs_product_img( 'garden-power-tools-hedge-trimmer-coffs-coast.jpg' ) ); ?>" alt="" width="600" height="400" loading="lazy" decoding="async">
+			<a class="product-card" href="<?php echo esc_url( home_url( '/garden-power-tools' ) ); ?>" style="background-image: url('<?php echo esc_url( coffs_product_img( 'garden-power-tools-hedge-trimmer-coffs-coast.jpg' ) ); ?>')">
 				<span class="product-card__label">Garden power tools</span>
 			</a>
 
-			<a class="product-card" href="<?php echo esc_url( home_url( '/battery-electric' ) ); ?>">
-				<img class="product-card__img" src="<?php echo esc_url( coffs_product_img( 'battery-electric-outdoor-power-equipment-coffs-coast.jpg' ) ); ?>" alt="" width="600" height="400" loading="lazy" decoding="async">
+			<a class="product-card" href="<?php echo esc_url( home_url( '/battery-electric' ) ); ?>" style="background-image: url('<?php echo esc_url( coffs_product_img( 'battery-electric-outdoor-power-equipment-coffs-coast.jpg' ) ); ?>')">
 				<span class="product-card__label">Battery &amp; electric</span>
 			</a>
 
